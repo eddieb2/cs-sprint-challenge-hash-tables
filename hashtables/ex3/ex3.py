abdb2343a1a2ac09c6c7aecb8c0e649bf7e01ad7
Edward Blanciak <blanciak.edward2@gmail.com>
2020-09-04 13:32:17 -0400

    ex4 complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,61 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    cache = {}
+    # store all array nums in dict key= num value = count
+    # if num already exists in cache increment the count
+    print(arrays)
+    # else add it to the cache and icrement the count
+
 
     return result
 
 
 if __name__ == "__main__":
-    arrays = []
+    # arrays = []
+    #
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # print(intersection(arrays))
+    intersection([
+        [1, 2, 3],
+        [1, 4, 5],
+        [1, 6, 7]
+    ])
+'''
+# Intersections of Multiple Lists
 
-    print(intersection(arrays))
+Find the intersection between multiple lists of integers.
+
+Do not use numpy or sets to solve this; use `dict` or hashtables,
+please.
+
+We're given a list of lists that contain integers:
+
+```python
+[
+    [1, 2, 3, 4, 5],
+    [12, 7, 2, 9, 1],
+    [99, 2, 7, 1,]
+]
+```
+
+And we need to compute the _intersection_, that is, numbers that exist
+in all lists.
+
+From the above input, the return value will be:
+
+```
+[1, 2]
+```
+
+Because those are the numbers that exist in all the lists.
+
+(Output can be in any order.)
+
+Limits:
+
+* There can be up to 10 lists in the list of lists.
+* The lists can contain up to roughly 1,000,000 elements each.
+'''
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,34 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+cache = {}
 
+def has_negatives(a):
+    result = []
+
+    for num in a:
+        # if num is positive add to cache w/ value 0
+        if num > 0:
+            # add positive num to cache
+            cache[num] = 0
+    print(cache)
+    for num in a:
+        # if num is negative, add num as the value of it's opposite key
+        if num < 0:
+            # check if it's opp is in the cache
+            if -(num) in cache:
+                # add the neg num as the value of positive key
+                cache[-(num)] = num
+            else:
+                cache[num] = 0
+
+    # if key's value is not a 0, add key to result list
+    for key, value in cache.items():
+        if value != 0:
+            result.append(key)
+
+    # print(len(result))
     return result
 
-
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+    print(has_negatives([1,2,3,-4]))
+
+    print(cache)
